a79d0efa9945975f0151ec4e5ba5957d43af1830
Rohan Kumar <rohan.shuvam@gmail.com>
2021-10-04 20:51:29 +0530

    Added search for KAA and GOGO

---
+++ pages/api/getanime.js
@@ -0,0 +1,28 @@
+import {search} from "kaa-scrapper";
+import {Gogo} from "gogo-site-scrapper";
+const gogo = new Gogo("https://gogoanime.pe");
+
+export default async function getAnime(req,res){
+    const result = await combiner(req.query.q);
+    res.status(200).json(result);
+}
+
+async function combiner(query){
+    console.log(query);
+    const kaa = await getKAAAnime(query);
+    const gogo = await getGoGoAnime(query);
+    const res = {
+        kaa: kaa,
+        gogo: gogo
+    }
+    return res;
+}
+async function getKAAAnime(query){
+    const result = await search(query);
+    return result;
+}
+
+async function getGoGoAnime(query){
+    const result = await gogo.searchAnime(query);
+    return result;
+}
