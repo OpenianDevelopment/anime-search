e917c75341134416d3a46214bf271ad0573acd3e
NORO3618 <66942330+Noro95@users.noreply.github.com>
2021-10-22 23:26:59 +0200

    Unfinished - 10/22
    Adding themes, changing the index looks and other things that i don't remember.
    
    P.S. it's fucked up rn and that's enough bugs for today

--- pages/api/getanime.js
+++ pages/api/getanime.js
@@ -1,28 +1,28 @@
-import {search} from "kaa-scrapper";
-import {Gogo} from "gogo-site-scrapper";
+import { search } from "kaa-scrapper";
+import { Gogo } from "gogo-site-scrapper";
+
 const gogo = new Gogo("https://gogoanime.pe");
 
-export default async function getAnime(req,res){
+export default async function getAnime(req, res) {
     const result = await combiner(req.query.q);
-    res.status(200).json(result);
+    res.status(200).json(JSON.stringify(result, null, "\t"));
 }
 
-async function combiner(query){
-    console.log(query);
+async function combiner(query) {
     const kaa = await getKAAAnime(query);
     const gogo = await getGoGoAnime(query);
     const res = {
         kaa: kaa,
-        gogo: gogo
+        gogo: gogo,
     }
     return res;
 }
-async function getKAAAnime(query){
+async function getKAAAnime(query) {
     const result = await search(query);
     return result;
 }
 
-async function getGoGoAnime(query){
+async function getGoGoAnime(query) {
     const result = await gogo.searchAnime(query);
     return result;
 }
